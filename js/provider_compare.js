964c77f9c092d53944016243a2a7f7d255014e30
Arjun Srinivasan <arjun.srinivasan.10@gmail.com>
2015-04-25 19:05:06 -0400

    new UI

--- js/provider_compare.js
+++ js/provider_compare.js
@@ -2,13 +2,19 @@ function compareProviders(){
 	var provider1 = $("#provider1").val();
 	var provider2 = $("#provider2").val();
 	
-	var provider1LabCount = {},provider2LabCount={},provider1DrugCount={},provider2DrugCount={},provider1ConsulCount={},provider2ConsulCount={};
+	var provider1LabCount = {},provider2LabCount={},provider1DrugCount={},provider2DrugCount={},provider1ConsulCount={},provider2ConsulCount={},provider1EncounterCount={},provider2EncounterCount={},provider1PatientCount={},provider2PatientCount={};
 	provider1LabCount[provider1]=0; 
 	provider2LabCount[provider2]=0; 
 	provider1DrugCount[provider1]=0; 
 	provider2DrugCount[provider2]=0; 
 	provider1ConsulCount[provider1]=0; 
 	provider2ConsulCount[provider2]=0; 
+	provider1EncounterCount[provider1]=0; 
+	provider2EncounterCount[provider2]=0; 
+	provider1PatientCount[provider1]=0; 
+	provider2PatientCount[provider2]=0; 
+	var coveredPatientsForP1 = [];
+	var coveredPatientsForP2 = [];
 	for(var i=0;i<allEncounters.length;i++){
 		if(allEncounters[i].providerId==provider1){
 			if(allEncounters[i].drugPrescribed!=""){
@@ -18,6 +24,7 @@ function compareProviders(){
 			}else if(allEncounters[i].consultOrdered!=""){
 				provider1ConsulCount[provider1]+=1;
 			}
+			provider1EncounterCount[provider1] += 1
 		}else if(allEncounters[i].providerId==provider2){
 			if(allEncounters[i].drugPrescribed!=""){
 				provider2DrugCount[provider2]+=1;
@@ -26,11 +33,12 @@ function compareProviders(){
 			}else if(allEncounters[i].consultOrdered!=""){
 				provider2ConsulCount[provider2]+=1;
 			}
+			provider2EncounterCount[provider2] += 1
 		}
 	}
 
-	var formattedDataMap = [{"key":providerIdNameMap[provider1],"color":"#1f77b4","values":[{"label":"Drugs Prescribed","value":provider1DrugCount[provider1]},{"label":"Lab Tests","value":provider1LabCount[provider1]},{"label":"Consults ordered","value":provider1ConsulCount[provider1]}]},
-							{"key":providerIdNameMap[provider2],"color":"#4DB84D","values":[{"label":"Drugs Prescribed","value":provider2DrugCount[provider2]},{"label":"Lab Tests","value":provider2LabCount[provider2]},{"label":"Consults ordered","value":provider2ConsulCount[provider2]}]}];
+	var formattedDataMap = [{"key":providerIdNameMap[provider1],"color":"#1f77b4","values":[{"label":"Total encounters","value":provider1EncounterCount[provider1]},{"label":"Drugs Prescribed","value":provider1DrugCount[provider1]},{"label":"Lab Tests","value":provider1LabCount[provider1]},{"label":"Consults ordered","value":provider1ConsulCount[provider1]}]},
+							{"key":providerIdNameMap[provider2],"color":"#4DB84D","values":[{"label":"Total encounters","value":provider2EncounterCount[provider2]},{"label":"Drugs Prescribed","value":provider2DrugCount[provider2]},{"label":"Lab Tests","value":provider2LabCount[provider2]},{"label":"Consults ordered","value":provider2ConsulCount[provider2]}]}];
 	drawProviderComparison(formattedDataMap);
 }
 
